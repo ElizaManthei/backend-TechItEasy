932523bd8fbebe7f8b62d0b189ee6bdd19807215
ElizaManthei <mazilemka@gmail.com>
2023-03-12 21:05:41 +0100

    niets

--- src/main/java/nl/novi/TechItEasy/Controllers/TelevisionsController.java
+++ src/main/java/nl/novi/TechItEasy/Controllers/TelevisionsController.java
@@ -9,20 +9,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("tvs")
 public class TelevisionsController {
-//private List<Tv> tvsDataBase = new ArrayList<>();
-
-//    @GetMapping("")
-//    public ResponseEntity<Object> getTvs () {
-//        return new ResponseEntity<>(tvsDataBase, HttpStatus.OK);
-//    }
-
 
     @Autowired
     TvRepository tvRepository;
@@ -77,39 +68,5 @@ public class TelevisionsController {
        tvRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
-//    @GetMapping("/{id}")
-//public ResponseEntity<Object> getTvById(@PathVariable int id) {
-//    if (id < 0 || id >= tvsDataBase.size()) { // determine if id exists
-//        throw new RecordNotFoundException("ID cannot be found");
-//    }
-//        return ResponseEntity.ok().body(tvsDataBase.get(id));
-//}
 
-//    @PostMapping("")
-//    public ResponseEntity<Tv> addTv (@RequestBody Tv ntv) {
-//    if (ntv.name.length() > 20) {
-//        throw new RecordTooLongException("The name is too long");
-//    }
-//        tvsDataBase.add(ntv);
-////    return new ResponseEntity<>( ntv, HttpStatus.CREATED);
-//      return   ResponseEntity.created(null).body(ntv);
-//}
-
-//@PutMapping("/{id}")
-//    public ResponseEntity<Object> updateTv(@PathVariable int id, @RequestBody Tv tv) {
-//        if (id < 0 || id >= tvsDataBase.size()) {
-//            throw new RecordNotFoundException("ID cannot be found");
-//        }
-//        tvsDataBase.set(id, tv);
-//        return ResponseEntity.ok( tvsDataBase.get(id));
-//}
-//@DeleteMapping("/{id}")
-//    public ResponseEntity<Object> deleteTvById (@PathVariable int id) {
-//    if (id < 0 || id >= tvsDataBase.size()) {
-//        throw new RecordNotFoundException("ID cannot be found");
-//    }
-//    tvsDataBase.remove(id);
-//
-//    return ResponseEntity.noContent().build();
-//}
 }
--- src/main/java/nl/novi/TechItEasy/Repositories/TvRepository.java
+++ src/main/java/nl/novi/TechItEasy/Repositories/TvRepository.java
@@ -1,7 +1,5 @@
 package nl.novi.TechItEasy.Repositories;
-
 import nl.novi.TechItEasy.Models.Tv;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 public interface TvRepository extends JpaRepository<Tv, Long> {
 }
